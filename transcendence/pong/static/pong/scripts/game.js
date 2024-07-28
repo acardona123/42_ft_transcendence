e933476668c7153e3b701075a45126ff45022d85
acardona <42.alex.cardona@gmail.com>
2024-07-29 01:46:24 +0200

    feat: introducing game settings

--- transcendence/pong/static/pong/scripts/game.js
+++ transcendence/pong/static/pong/scripts/game.js
@@ -1,7 +1,11 @@
 
 var config = {
+	parent: "phaser_pong_game", //html DOM element or id
 	width: 256,
 	height: 272,
+
+	title: "custom_pong",
+	version: 0.1,
 	backgroundColor: 0x000000,
 	scene: [],
 	pixelArt: true,
@@ -10,7 +14,12 @@ var config = {
 		arcade: {
 			debug: false
 		}
-	}
+	},
+	autoFocus: true,
+	disableContextMenu: false,//set to true to disable right click menu
+	//audio:
+	autocenter: Phaser.Scale.CENTER_BOTH,
+	
 }
 
 var gameSettings = {
