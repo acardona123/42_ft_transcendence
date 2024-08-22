79f14f853209ece857dfcbbc2d145cb098b940f8
J0hann3 <jvigny@student.42.fr>
2024-08-22 19:15:06 +0200

    feat: update friends vault to create database

--- backend/friends/app/vault/vault_init.py
+++ backend/friends/app/vault/vault_init.py
@@ -6,16 +6,7 @@ VAULT_ENV_FILE = '/usr/src/app/vault/.env'
 
 os.environ['REQUESTS_CA_BUNDLE'] = '/usr/src/app/vault/vault.crt'
 
-VAULT_CLIENT = initialize()
-
-VAULT_CLIENT = unsealed(VAULT_CLIENT)
-
-load_dotenv(dotenv_path=VAULT_ENV_FILE, override=True)
-
-if VAULT_CLIENT.sys.is_initialized() == True \
-	and VAULT_CLIENT.sys.is_sealed() == False \
- 	and VAULT_CLIENT.is_authenticated() == False:
-	VAULT_CLIENT.token = os.getenv('VAULT_TOKEN')
+VAULT_CLIENT = create_client()
 
 enable_database(VAULT_CLIENT)
 configure_database(VAULT_CLIENT, os.getenv("VAULT_DATABASE_NAME"))
