71f895ceb9e15b61555159456dff4a455ab281c5
J0hann3 <jvigny@student.42.fr>
2024-08-24 17:23:51 +0200

    fix: copy vault crt to bind volume

--- backend/vault/vault_init.sh
+++ backend/vault/vault_init.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/sh
 set -e
 
+#add crt to volume
+cp /vault/tls/vault.crt /vault/env/
+
 vault server -config=/vault/config/config.hcl &
 pid=$!
 
@@ -30,7 +33,7 @@ else
 
 	#log in vault
 	echo 'Log in vault server ...'
-	ROOT_TOKEN=$(grep 'Initial Root Token:' /vault/file/keys | awk '{print $NF}');
+	export ROOT_TOKEN=$(grep 'Initial Root Token:' /vault/file/keys | awk '{print $NF}');
 	vault login $ROOT_TOKEN 2>&1 > /dev/null;
 
 	#update .env with VAULT_TOKEN
