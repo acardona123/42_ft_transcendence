4c947ddccc19a27ed82824423c2c4a0f1c535566
QuentinThierry <qthierry2@gmail.com>
2024-08-30 21:14:35 +0200

    feat: quit popup on click outside of the popup or escape key

--- friends.js
+++ friends.js
@@ -126,7 +126,7 @@ function construct_friend_list()
 
 function position_friend_popup(popup_add_friend)
 {
-	const add_friend_button = document.getElementById("add-friend-button");
+	const add_friend_button = document.getElementById("add_friend_button");
 	var rect = add_friend_button.getBoundingClientRect();
 
 	const offsetX = rect.right + 10;
@@ -150,39 +150,57 @@ function fill_add_popup(popup_add_friend)
 	popup_add_friend.appendChild(button_add);
 }
 
-function click_button_add_friend(event)
+function show_friend_popup(event)
 {
 	if (event.key != "Enter" && event.key != null)
 		return ;
-	deactivate_friends_button();
 	const popup_add_friend = document.createElement('div');
 	popup_add_friend.id = "popup_add_friend";
 	popup_add_friend.className = "popup-add-friend";
 	popup_add_friend.style.zIndex = "1";
-
+	popup_add_friend.onkeydown = remove_friends_popup;
+	
 	position_friend_popup(popup_add_friend);
 	fill_add_popup(popup_add_friend);
-
+	
 	const friend_text_title = document.getElementById("friend-title");
 	friend_text_title.appendChild(popup_add_friend);
+	document.getElementById("add_friend_pseudo_input").focus();
+	deactivate_friends_button();
+}
+
+function remove_friends_popup(event)
+{
+	if (event) // triggered by event keyboard
+	{
+		if (event.key != "Escape")
+			return;
+	}
+	document.getElementById("popup_add_friend")?.remove();
+	activate_friends_button();
 }
 
 function activate_friends_button()
 {
-	const elem = document.getElementById("add-friend-button");
-	elem.onclick = click_button_add_friend;
-	elem.onkeydown = click_button_add_friend;
+	const elem = document.getElementById("add_friend_button");
+	elem.onclick = show_friend_popup;
+	elem.onkeydown = show_friend_popup;
+	friend_popup_just_popped = true;
 }
+
+var friend_popup_just_popped = false;
 
 function deactivate_friends_button()
 {
-	const elem = document.getElementById("add-friend-button");
+	const elem = document.getElementById("add_friend_button");
 	elem.onclick = null;
+	elem.onkeydown = null;
+	friend_popup_just_popped = true;
 }
 
 function onWindowResize()
 {
-	if (document.getElementById("add-friend-button").onclick == null)
+	if (is_friend_popup_activated())
 	{
 		const popup_add_friend = document.getElementById("popup_add_friend");
 		position_friend_popup(popup_add_friend);
@@ -190,5 +208,19 @@ function onWindowResize()
 }
 
 window.onresize = onWindowResize;
-activate_friends_button();
+const elem = document.getElementById("add_friend_button");
+elem.onclick = null;
 construct_friend_list();
+window.addEventListener('click', (event) =>
+{
+	if (friend_popup_just_popped)
+	{
+		friend_popup_just_popped = false;
+		return;
+	}
+	if (!document.getElementById("popup_add_friend")?.contains(event.target))
+	{
+		remove_friends_popup();
+	}
+})
+activate_friends_button();
