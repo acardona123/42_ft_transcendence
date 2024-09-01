42b46ee46214e45b13c63bf5378aa2499618d9d2
QuentinThierry <qthierry2@gmail.com>
2024-09-01 15:22:08 +0200

    refactor: changed add_friend functions to be used by accept_request

--- friends_list.js
+++ friends_list.js
@@ -1,17 +1,17 @@
-const DBfriendList = [
-	{pseudo: "chat mechant", online: false, picture: "./img/tiger.jpeg"},
-	{pseudo: "kitty", online: true, picture: "./img/dog.webp"},
-	{pseudo: "gros chat", online: true, picture: "./img/tiger.jpeg"},
-	{pseudo: "rose", online: true, picture: "./img/flower.jpeg"},
-	{pseudo: "tigre", online: false, picture: "./img/tiger.jpeg"},
-	{pseudo: "jordi", online: true, picture: "./img/dog.webp"},
-	{pseudo: "fleur", online: false, picture: "./img/flower.jpeg"}
+const DB_friend_list = [
+	{pseudo: "chat mechant", is_online: false, picture: "./img/tiger.jpeg"},
+	{pseudo: "kitty", is_online: true, picture: "./img/dog.webp"},
+	{pseudo: "gros chat", is_online: true, picture: "./img/tiger.jpeg"},
+	{pseudo: "rose", is_online: true, picture: "./img/flower.jpeg"},
+	{pseudo: "tigre", is_online: false, picture: "./img/tiger.jpeg"},
+	{pseudo: "jordi", is_online: true, picture: "./img/dog.webp"},
+	{pseudo: "fleur", is_online: false, picture: "./img/flower.jpeg"}
 ]
 
-function getFriendProfilPic(friendInfo) {
+function getFriendProfilPic(picture) {
 	const profil_pic = document.createElement("img");
 	profil_pic.className = "profil-pic";
-	profil_pic.src = friendInfo.picture;
+	profil_pic.src = picture;
 	return profil_pic;
 }
 
@@ -24,22 +24,22 @@ function getFriendDiv()
 	return div_object;
 }
 
-function getFriendPseudo(friendInfo)
+function getFriendPseudo(pseudo)
 {
 	const text = document.createElement('p');
 	text.className = "friend-text";
-	text.textContent = friendInfo.pseudo;
+	text.textContent = pseudo;
 	return text;
 }
 
-function getFriendOnline(friendInfo)
+function getFriendOnline(f_is_online)
 {
-	const isOnline = friendInfo.online;
+	const is_online = f_is_online;
 	const online_dot_div = document.createElement('div');
 	online_dot_div.style.position = "relative"
 
 	const online_dot_img = document.createElement('img');
-	online_dot_img.src = isOnline ? "./img/online.png" : "./img/offline.png";
+	online_dot_img.src = is_online ? "./img/online.png" : "./img/offline.png";
 	online_dot_img.className = "friend-online-dot";
 	online_dot_img.onclick = remove_friend;
 
@@ -68,59 +68,64 @@ function remove_friend(event)
 	const pseudoToRemove = elem.parentNode.parentNode.childNodes[1].textContent;
 	// DELETE on database
 	// since no databse yet, do :
-	for (let i = 0; i < DBfriendList.length; i++)
+	for (let i = 0; i < DB_friend_list.length; i++)
 	{
-		if (pseudoToRemove == DBfriendList[i].pseudo)
+		if (pseudoToRemove == DB_friend_list[i].pseudo)
 		{
-			DBfriendList.splice(i, 1);
+			DB_friend_list.splice(i, 1);
 			break;
 		}
 	}
 	elem.parentNode.parentNode.remove()
 }
 
-function getFriendList()
+function add_friend(pseudo, is_online, picture)
 {
-	const onlineFriendList = [];
-	const offlineFriendList = [];
-
-	for (let i = 0; i < DBfriendList.length; i++)
-	{
-		if (DBfriendList[i].online)
-			onlineFriendList.push(DBfriendList[i]);
-		else
-			offlineFriendList.push(DBfriendList[i]);
-	}
-
-	onlineFriendList.sort((a, b) =>
-	{
-		const name1 = a.pseudo.toLowerCase();
-		const name2 = b.pseudo.toLowerCase();
-		return name1.localeCompare(name2);
-	});
-
-	offlineFriendList.sort((a, b) =>
-	{
-		const name1 = a.pseudo.toLowerCase();
-		const name2 = b.pseudo.toLowerCase();
-		return name1.localeCompare(name2);
-	});
-
-	return onlineFriendList.concat(offlineFriendList);
+	const newElement = getFriendDiv();
+	newElement.appendChild(getFriendProfilPic(picture));
+	newElement.appendChild(getFriendPseudo(pseudo));
+	newElement.appendChild(getFriendOnline(is_online));
+	return newElement;
 }
 
-const friendList = getFriendList();
+function get_friend_list()
+{
+	const online_friend_list = [];
+	const offline_friend_list = [];
+
+	for (let i = 0; i < DB_friend_list.length; i++)
+	{
+		if (DB_friend_list[i].online)
+			online_friend_list.push(DB_friend_list[i]);
+		else
+			offline_friend_list.push(DB_friend_list[i]);
+	}
+
+	online_friend_list.sort((a, b) =>
+	{
+		const name1 = a.pseudo.toLowerCase();
+		const name2 = b.pseudo.toLowerCase();
+		return name1.localeCompare(name2);
+	});
+
+	offline_friend_list.sort((a, b) =>
+	{
+		const name1 = a.pseudo.toLowerCase();
+		const name2 = b.pseudo.toLowerCase();
+		return name1.localeCompare(name2);
+	});
+
+	return online_friend_list.concat(offline_friend_list);
+}
 
 function construct_friend_list()
 {
-	for (let i = 0; i < DBfriendList.length; i++)
+	const friend_list = get_friend_list();
+	for (let i = 0; i < friend_list.length; i++)
 	{
 		const friends_list = document.getElementById('friends-list');
-		const newElement = getFriendDiv(friendList[i]);
-		newElement.appendChild(getFriendProfilPic(friendList[i]));
-		newElement.appendChild(getFriendPseudo(friendList[i]));
-		newElement.appendChild(getFriendOnline(friendList[i]));
-		friends_list.appendChild(newElement);
+		const new_friend = add_friend(friend_list[i].pseudo, friend_list[i].is_online, friend_list[i].picture)
+		friends_list.appendChild(new_friend);
 	}
 }
 
