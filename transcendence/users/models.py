15a573bbd26f2d2556e89bf2d7e975f5870ff4cc
Alexandre <alexandremartinez42@gmail.com>
2024-09-02 16:29:05 +0200

    feat(user) : remove admin

--- transcendence/users/models.py
+++ transcendence/users/models.py
@@ -1,19 +1,17 @@
 from django.db import models
 from django.core.validators import FileExtensionValidator
 
-from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
-from django.utils import timezone
-from django.utils.translation import gettext_lazy as _
+from django.contrib.auth.models import AbstractBaseUser
 
 from .managers import CustomUserManager
 
 # there is hidden an id 
-class CustomUser(AbstractBaseUser, PermissionsMixin):
-    email = models.EmailField(_("email address"), unique=True)
+class CustomUser(AbstractBaseUser):
+    email = models.EmailField("email address", unique=True)
     pseudo = models.CharField(max_length=20, unique=True)
     phone = models.CharField(max_length=12, unique=True)
     profile_picture = models.ImageField(
-        _("profile picture"),
+        "profile picture",
         default='default_profile_picture.jpg',
         upload_to='profile_pictures/',
         validators=[FileExtensionValidator(allowed_extensions=['jpg', 'jpeg', 'png'])]
