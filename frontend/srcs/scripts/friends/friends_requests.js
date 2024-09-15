c46854bd2beacabeec58a1f147d4ee309326340b
J0hann3 <jvigny@student.42.fr>
2024-09-15 16:28:51 +0200

    feat: integration for friend requests accept and reject

--- frontend/srcs/scripts/friends/friends_requests.js
+++ frontend/srcs/scripts/friends/friends_requests.js
@@ -1,15 +1,15 @@
 const g_requests = [
-	{pseudo : "7", date :  7},
-	{pseudo : "2", date :  2},
-	{pseudo : "8", date :  8},
-	{pseudo : "4", date :  4},
-	{pseudo : "6", date :  6},
-	{pseudo : "9", date :  9},
-	{pseudo : "1", date :  1},
-	{pseudo : "11", date : 11},
-	{pseudo : "10", date : 10},
-	{pseudo : "3", date :  3},
-	{pseudo : "5", date :  5}
+	{pseudo : "7"/*, date :  7*/},
+	{pseudo : "2"/*, date :  2*/},
+	{pseudo : "8"/*, date :  8*/},
+	{pseudo : "4"/*, date :  4*/},
+	{pseudo : "6"/*, date :  6*/},
+	{pseudo : "9"/*, date :  9*/},
+	{pseudo : "1"/*, date :  1*/},
+	{pseudo : "11"/*, date : 11*/},
+	{pseudo : "10"/*, date : 10*/},
+	{pseudo : "3"/*, date :  3*/},
+	{pseudo : "5"/*, date :  5*/}
 ];
 
 function send_friend_request(event)
@@ -20,18 +20,34 @@ function send_friend_request(event)
 	remove_friends_popup();
 }
 
-function accept_friend_req(event)
+async function accept_friend_req(event)
 {
 	const accepted_pseudo = event.target.previousSibling.textContent;
+	const url = "https://localhost:8443/api/friends/request/" + data_requests.find(o => o.username === accepted_pseudo).id + "/";
 
-	tmp_array = ["./img/tiger.jpeg", "./img/dog.webp", "./img/flower.jpeg"];
-	const is_online = Math.random() <= 0.5;
-	const picture = tmp_array[Math.floor(Math.random() * tmp_array.length)];
-	add_friend(accepted_pseudo, is_online, picture);
-	DB_friend_list.push({pseudo: accepted_pseudo, is_online: is_online, picture: picture})
-	update_friend_list();
+	try
+	{
+		const response = await fetch(url, {method: 'POST'});
+		if (!response.ok)
+		{
+			throw new Error(`Response status: ${data.status}`);
+		}
+		// TODO: update friend request with the body returned
+	}
+	catch (error)
+	{
+		// TODO: popup error
+		console.log("Friend request accept failed: " + error.message);
+		return ;
+	}
+	// tmp_array = ["./img/tiger.jpeg", "./img/dog.webp", "./img/flower.jpeg"];
+	// const is_online = Math.random() <= 0.5;
+	// const picture = tmp_array[Math.floor(Math.random() * tmp_array.length)];
+	// add_friend(accepted_pseudo, is_online, picture);
+	// DB_friend_list.push({pseudo: accepted_pseudo, is_online: is_online, picture: picture})
+	// update_friend_list();
 
-	remove_req_sub_container_div(accepted_pseudo);
+	// remove_req_sub_container_div(accepted_pseudo);
 	// send to back
 }
 
@@ -42,7 +58,7 @@ function reject_friend_req(event)
 	// send to back
 }
 
-function remove_req_sub_container_div(pseudo_to_remove)
+async function remove_req_sub_container_div(pseudo_to_remove)
 {
 	const req_container = document.getElementById("req_container");
 
@@ -50,39 +66,40 @@ function remove_req_sub_container_div(pseudo_to_remove)
 	{
 		if (req_container.childNodes[i].childNodes[0].textContent == pseudo_to_remove)
 		{
-			req_container.childNodes[i].remove();
-			// DELETE on database
-			// since no databse yet, do :
-
-			for (let j = 0; j < g_requests.length; j++)
+			try
 			{
-				if (pseudo_to_remove == g_requests[j].pseudo)
+				const url = "https://localhost:8443/api/friends/request/" + data_requests.find(o => o.username === pseudo_to_remove).id + "/";
+				const response = await fetch(url, {method:'DELETE'});
+				if (!response.ok)
 				{
-					g_requests.splice(j, 1);
-					break;
+					throw new Error(`Response status: ${data.status}`);
 				}
+				// TODO: update friend request with the body returned
 			}
-
+			catch (error)
+			{
+				// TODO: popup error
+				console.log("Friend request reject failed: " + error.message);
+				return ;
+			}
 			break ;
 		}
 	}
-	if (req_container.childNodes.length == 0)
-		update_requests_list();
 }
 
 function get_requests_pseudo()
 {
 	// constant array since there is no back yet
-	const requests = [...g_requests];
+	const requests = [...data_requests];
 
-	requests.sort((a, b) => {
-		if (a.date < b.date)
-			return -1;
-		else
-			return 1;
-	});
+	// requests.sort((a, b) => {
+	// 	if (a.date < b.date)
+	// 		return -1;
+	// 	else
+	// 		return 1;
+	// });
 
-	return requests.map(req => req.pseudo);
+	return requests.map(req => req.username);
 }
 
 function get_req_sub_container(request_info)
@@ -129,7 +146,7 @@ function update_requests_list(is_init)
 	
 	if (!is_init)
 		empty_requests_list();
-
+	
 	if (requests_pseudo.length == 0)
 	{
 		const empty_text = document.createElement('p');
@@ -143,7 +160,30 @@ function update_requests_list(is_init)
 		const req_sub_container = get_req_sub_container(requests_pseudo[i]);
 		container.appendChild(req_sub_container);
 	}
-
 }
 
-update_requests_list();
+async function get_data_from_database()
+{
+	try {
+		let data = await fetch("https://localhost:8443/api/friends/request/");
+		if (!data.ok)
+		{
+			throw new Error(`Response status: ${data.status}`);
+		}
+		body = await data.json();
+		return body;
+	}
+	catch (error)
+	{
+		// TODO: popup error
+		console.log("Friend request fetch failed: " + error.message);
+	}
+	return undefined;
+}
+
+let data_requests = undefined;
+(async () => {
+	data_requests = await get_data_from_database();
+	if (data_requests != undefined)
+		update_requests_list();
+})()
