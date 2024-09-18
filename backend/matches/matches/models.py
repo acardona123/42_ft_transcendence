f890505983c0684e58d091d9bb8841b8121237cc
Alex Cardona <42.alex.cardona@gmail.com>
2024-09-19 00:37:47 +0200

    feat: adding a serializer for the Match model

--- backend/matches/matches/models.py
+++ backend/matches/matches/models.py
@@ -1,8 +1,5 @@
 from django.db import models
 
-class Game(models.Model):
-	name = models.CharField(max_length = 20)
-
 GAME = {
 	"FB": "Flappy Fish",
 	"PG": "Pong"
---
+++ backend/matches/matches/serializer.py
@@ -0,0 +1,101 @@
+
+
+from rest_framework import serializers
+from .models import Match
+from django.core.exceptions import BadRequest
+
+from django.conf import settings
+import requests
+
+class DynamicFieldsModelSerializer(serializers.ModelSerializer):
+	def __init__(self, *args, **kwargs):
+		fields = kwargs.pop('fields', None)
+
+		super().__init__(*args, **kwargs)
+
+		if fields is not None:
+			allowed = set(fields)
+			existing = set(self.fields)
+			for field_name in existing - allowed:
+				self.fields.pop(field_name)
+
+
+class MatchSerializer(DynamicFieldsModelSerializer):
+	main_player_id = serializers.SerializerMethodField()
+	opponent_id = serializers.SerializerMethodField()
+	main_player_username = serializers.SerializerMethodField()
+	opponent_username = serializers.SerializerMethodField()
+	main_player_score = serializers.SerializerMethodField()
+	opponent_score = serializers.SerializerMethodField()
+
+	class Meta:
+		model = Match
+		fields = ['id', 'game', 'max_score', 'max_duration', 'date', 'duration', 'main_player_id', 'opponent_id', 'main_player_username', 'opponent_username', 'main_player_score', 'opponent_score']
+
+
+	def __init__(self, *args, **kwargs):
+		super().__init__(*args, **kwargs)
+
+		if hasattr(self, 'instance') and 'opponent_username' in self.fields:
+			opponents_ids = list()
+			for match in self.instance:
+				if match.user1 == self.context['user_id']:
+					opponents_ids.append(match.user2)
+				else:
+					opponents_ids.append(match.user1)
+			self.opponents_id_map = self.get_usernames_request(opponents_ids)
+		else:
+			self.opponents_id_map = {}
+
+
+	def get_main_player_id(self, obj):
+		return self.context['user_id']
+
+	def get_opponent_id(self, obj):
+		if self.context['user_id'] == obj.user1:
+			return obj.user2
+		else:
+			return obj.user1
+
+	def get_main_player_score(self, obj):
+		if self.context['user_id'] == obj.user1:
+			return obj.score1
+		else:
+			return obj.score2
+
+	def get_opponent_score(self, obj):
+		if self.context['user_id'] == obj.user1:
+			return obj.score2
+		else:
+			return obj.score1
+
+	def get_main_player_username(self, obj):
+		if "main_player_username" in self.fields:
+			id_main_player = self.context['user_id']
+			return self.get_usernames_request([id_main_player]).get(str(id_main_player));
+		else:
+			return ""
+	
+	def get_opponent_username(self, obj):
+		if self.context['user_id'] == obj.user1:
+			return self.opponents_id_map.get(str(obj.user2))
+		else:
+			return self.opponents_id_map.get(str(obj.user1))
+
+
+	# def get_usernames_request(self, users_id):
+	# 	url = f"{settings.USERS_MICROSERVICE_URL}/api/users/usernames/"
+	# 	response = requests.post(url, json={'users' : users_id})
+	# 	if response.status_code != 200:
+	# 		raise BadRequest
+	# 	return response.json()
+
+
+	def get_usernames_request(self, users_id):
+		usernames = {};
+		for user in users_id:
+			id = str(user)
+			name = "username_id_" + id
+			usernames[id] = name
+		return usernames
+
