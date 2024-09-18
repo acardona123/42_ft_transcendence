3a947a6ee659b09c98b589e0bf24d70b084cace6
QuentinThierry <qthierry2@gmail.com>
2024-09-18 15:11:39 +0200

    feat: rework how the timer works on popups to make it more reliable

--- frontend/srcs/scripts/popup/popup.js
+++ frontend/srcs/scripts/popup/popup.js
@@ -1,9 +1,10 @@
 let popup_list = [];
 const space_between_popups = 3; // in px
 
-async function delay(ms)
+async function delay(ms, print)
 {
-	// console.log(ms)
+	if (print)
+		console.log(ms)
 	return new Promise(res => setTimeout(res, ms));
 }
 
@@ -16,18 +17,18 @@ function update_all_popups_pos()
 	})
 }
 
-function create_popup(text, time_before_decay, time_to_decay, hex_color)
+function create_popup(text, t_before_decay, t_decay,
+					t_after_hover, hex_color)
 {
 	const popup_main = document.createElement('div');
 
 	popup_main.className = "popup-main-div";
-	// popup_main.id = "popup-" + popup_list.length.toString();
 	popup_main.onanimationend = remove_popup;
 	popup_main.onmouseenter = popup_mouse_enter;
 	popup_main.onmouseleave = popup_mouse_leave;
 	popup_id = popup_list.length;
-	popup_list.push({popup : popup_main, is_hover : false});
-	popup_handler(popup_id);
+	popup_list.push({popup : popup_main, is_hover : false, have_been_hover : false});
+	popup_handler(popup_id, t_before_decay, t_after_hover);
 	document.body.appendChild(popup_main);
 	update_all_popups_pos();
 }
@@ -36,6 +37,7 @@ function popup_mouse_enter()
 {
 	const popup_id = popup_list.findIndex(p => p.popup === this);
 	popup_list[popup_id].is_hover = true;
+	popup_list[popup_id].have_been_hover = true;
 }
 
 function popup_mouse_leave()
@@ -47,23 +49,22 @@ function popup_mouse_leave()
 function remove_popup()
 {
 	this.remove();
+	update_all_popups_pos();
 }
 
-async function popup_handler(popup_id)
+async function popup_handler(popup_id, t_before_decay, t_after_hover)
 {
-	should_redo = true;
+	await delay(t_before_decay);
+	let should_redo = true;
 	while (should_redo)
 	{
-		inner_loop = true;
 		should_redo = false;
-		let delay_end = delay(2000);
-		while (inner_loop)
+		if (popup_list[popup_id].is_hover || popup_list[popup_id].have_been_hover)
 		{
-			delay_end.then(() => {inner_loop = false});
-			if (popup_list[popup_id].is_hover)
-				should_redo = true;
-			await delay(100);
+			await delay(t_after_hover);
+			should_redo = true;
 		}
+		popup_list[popup_id].have_been_hover = false;
 	}
 	popup_list[popup_id].popup.style.animationPlayState = "running";
 }
