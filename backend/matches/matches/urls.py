4b386fdea000b4eb2ce98405091b446bb007dd95
Alex Cardona <42.alex.cardona@gmail.com>
2024-10-08 20:17:02 +0200

    fix: adding / at the end of urls to be compliant with nginx

--- backend/matches/matches/urls.py
+++ backend/matches/matches/urls.py
@@ -6,16 +6,16 @@ from .views_test import test_create_matches, test_hello, test_delete_all_matches
 urlpatterns = [
     # path("", list_matches, name='list_matches'),
     path("api/matches/", match_history, name='list_matches'),
-    path("api/matches/new/me-ai", new_match_against_ai, name='new_match_against_ai'),
-    path("api/matches/new/me-guest", new_match_against_guest, name='new_match_against_guest'),
-    path("api/matches/new/me-player", new_match_against_player, name='new_match_against_player'),
+    path("api/matches/new/me-ai/", new_match_against_ai, name='new_match_against_ai'),
+    path("api/matches/new/me-guest/", new_match_against_guest, name='new_match_against_guest'),
+    path("api/matches/new/me-player/", new_match_against_player, name='new_match_against_player'),
 
-    path("api/matches/test/hello", test_hello, name='test_hello'),
-	path("api/matches/test/create_bunk", test_create_matches, name='test_create_matches'),
-	path("api/matches/test/delete_all", test_delete_all_matches, name='test_delete_all_matches'),
-	path("api/matches/test/finish_all", test_finish_all_matches, name='test_finish_all_matches'),
+    path("api/matches/test/hello/", test_hello, name='test_hello'),
+	path("api/matches/test/create_bunk/", test_create_matches, name='test_create_matches'),
+	path("api/matches/test/delete_all/", test_delete_all_matches, name='test_delete_all_matches'),
+	path("api/matches/test/finish_all/", test_finish_all_matches, name='test_finish_all_matches'),
 
-	path("private_api/matches/new_match_verified_id", new_match_verified_id, name='new_match_verified_id'),
+	path("private_api/matches/new_match_verified_id/", new_match_verified_id, name='new_match_verified_id'),
 	
     path("api/matches/finish/<int:match_id>/", finish_match, name='finish_match'),
 
