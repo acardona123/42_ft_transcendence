c031939549283b943662cf6f02616b0efb562929
QuentinThierry <qthierry2@gmail.com>
2024-10-13 17:25:10 +0200

    feat: getting user information filled in inputs user from back

--- frontend/modal_contents/edit_profile/scripts/user_form.js
+++ frontend/modal_contents/edit_profile/scripts/user_form.js
@@ -50,12 +50,47 @@ function dfa_collapse()
 	dfa_is_visible = !dfa_is_visible;
 }
 
+async function get_user_informations()
+{
+	const url = "https://localhost:8443/api/users/update/user/";
+	let fetched_data = await fetch_with_token(url, {
+		method: 'GET',
+		headers: {}
+	});
+	if (!fetched_data.ok)
+		throw new Error("Error retrieving user informations.");
+	let data = await fetched_data.json();
+	return data.data;
+}
+
 function clear_edp_user_inputs()
 {
-	document.getElementById("edp-input-username").value = "";
-	document.getElementById("edp-input-email").value = "";
-	document.getElementById("edp-input-phone").value = "";
-	document.getElementById("edp-input-pin").value = "";
+	const save_button = document.getElementById("edp-save-informations-button");
+	const inputs = [
+		document.getElementById("edp-input-username"),
+		document.getElementById("edp-input-email"),
+		document.getElementById("edp-input-phone"),
+		document.getElementById("edp-input-pin")
+	];
+	save_button.disabled = true;
+	for (elem of inputs)
+	{
+		elem.value = "";
+		elem.disabled = true;
+	}
+	get_user_informations()
+		.then((infos) => {
+			save_button.disabled = false;
+			let i = 0;
+			for (field in infos)
+			{
+				inputs[i].value = infos[field];
+				inputs[i].disabled = false;
+				i++;
+			}
+		}).catch(() => {
+			create_popup("Error retrieving user informations.", 4000, 4000, HEX_RED, HEX_RED_HOVER);
+		});
 }
 
 function format_empty_values(body)
@@ -102,13 +137,12 @@ async function submit_user_form(form)
 			error_update_user_from_back(data);
 			return ;
 		}
-		console.log("Informations updated.");
-		// TODO: popup
+		create_popup("Informations updated.", 4000, 4000, HEX_GREEN, HEX_GREEN_HOVER);
 		return ;
 	}
 	catch (error)
 	{
-		// TODO: handle errors properly
+		create_popup(error, 4000, 4000, HEX_RED, HEX_RED_HOVER);
 		console.log(error);
 		return ;
 	}
