2e561bf49d96da4a8e6f7728ab18ae938c9bb061
Alex Cardona <42.alex.cardona@gmail.com>
2024-10-16 19:25:57 +0200

    style: putting the different messages and documentation variables in a separated document

--- backend/matches/matches/views_public_api.py
+++ backend/matches/matches/views_public_api.py
@@ -24,60 +24,7 @@ from drf_yasg import openapi
 from matches.views_users_requests import get_authenticated_user_id, get_authenticated_user_id, get_new_ai_request, get_new_guest_request, check_player_pin_ok
 from matches.views_tournaments_requests import is_host_of_tournament
 
-# -----------------------SWAGGER(DOC)---------------------------
-
-MSG_ERROR_UNIDENTIFIED_USER = 'can\'t access the historic without being identified as a player'
-MSG_ERROR_JSON_FORMAT = 'Expecting a json body for creating a new match'
-
-MSG_LIST_ERROR_SERIALIZER_DISPLAY = 'failed to display the matches history'
-MSG_LIST_SUCCESS = 'matches history'
-DOC_LIST_MATCHES = openapi.Response(
-			description=MSG_LIST_SUCCESS,
-			examples={
-				"application/json": {
-					"message": MSG_LIST_SUCCESS,
-					"matches": [
-						{
-						"game": "FB",
-						"date": "2024-10-15T17:18:12.666123Z",
-						"duration": 150,
-						"main_player_username": "username_id_7",
-						"opponent_username": "username_id_3",
-						"main_player_score": 15,
-						"opponent_score": 7
-						},
-						{
-						"game": "PG",
-						"date": "2024-10-15T17:18:12.666104Z",
-						"duration": 127,
-						"main_player_username": "username_id_7",
-						"opponent_username": "username_id_4",
-						"main_player_score": 8,
-						"opponent_score": 19
-						}
-					]
-				}
-			}
-		)
-
-MSG_NEW_MATCH_ERROR_MISSING_FIELD = 'match creation impossible: missing username or pin for the second player'
-MSG_NEW_MATCH_ERROR_WRONG_PIN = 'match creation impossible: wrong pin for the second player'
-
-MSG_FINISH_MATCH_ERROR_ID_NOT_FOUND = 'there is no match identified by the given id to be ended'
-MSG_FINISH_MATCH_ERROR_NOT_ALLOWED = 'a match can be declared as finished only by its players or the creator of the tournament'
-MSG_FINISH_MATCH_ERROR_ALREADY_FINISHED = 'the match was already finished'
-MSG_FINISH_MATCH_ERROR_MISSING_FIELD = 'missing "score1" or "score2" or "duration" field to successfully end a match'
-
-def doc_error_generation(err_description, err_msg):
-	return (openapi.Response(
-				description="Error: " + err_description,
-				examples={
-					"application/json": {
-						"message": err_msg
-					}
-				})
-			)
-
+from .doc import (MSG_ERROR_UNIDENTIFIED_USER, MSG_ERROR_JSON_FORMAT, MSG_LIST_ERROR_SERIALIZER_DISPLAY, MSG_LIST_SUCCESS, DOC_LIST_MATCHES, MSG_NEW_MATCH_ERROR_MISSING_FIELD, MSG_NEW_MATCH_ERROR_WRONG_PIN, MSG_FINISH_MATCH_ERROR_ID_NOT_FOUND, MSG_FINISH_MATCH_ERROR_NOT_ALLOWED, MSG_FINISH_MATCH_ERROR_ALREADY_FINISHED, MSG_FINISH_MATCH_ERROR_MISSING_FIELD, JWT_TOKEN, doc_error_generation)
 
 
 
