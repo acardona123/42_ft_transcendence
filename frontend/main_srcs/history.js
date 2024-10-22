17970e021e4b94c13962ba8030437d904aa53774
QuentinThierry <qthierry2@gmail.com>
2024-10-22 13:34:44 +0200

    feat: register to login and login to register button are now handle in window history

--- frontend/main_srcs/history.js
+++ frontend/main_srcs/history.js
@@ -5,16 +5,14 @@ function history_back_on_open(modal_id, funcs)
 	open_modal(modal_id, window[funcs.bf], window[funcs.af], false);
 }
 
-function history_back_on_close(modal_id, funcs)
+function history_back_on_close()
 {
 	if (modal_on_screen)
 		close_modal(modal_on_screen, undefined, false);
-	close_modal(modal_id, window[funcs?.bf], false);
 }
 
 function on_popstate_event(event)
 {
-	console.log(event.state);
 	if (!event.state)
 	{
 		if (modal_on_screen)
@@ -24,20 +22,7 @@ function on_popstate_event(event)
 	if (event.state.open)
 		history_back_on_open(event.state.modal_id, event.state.funcs);
 	else
-		history_back_on_close(event.state.modal_id, event.state.funcs);
+		history_back_on_close();
 }
 
-document.addEventListener("keydown", (event) =>
-{
-	if (event.key == "a")
-	{
-		console.log("printing:");
-		console.log(history.state);
-	}
-});
-
 window.addEventListener("popstate", on_popstate_event);
-
-window.addEventListener("beforeunload", function(e){
-
-});
