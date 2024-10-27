848cede7ed0e8651e0ccc66dd00cb6d0be39280b
Alexandre <alexandremartinez42@gmail.com>
2024-10-27 21:25:32 +0100

    stats(feat) : public api add , still need more verifiaction

--- backend/stats/stats/urls.py
+++ backend/stats/stats/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
 	path("private/create_statistics_user/", create_statistics_user, name="create_stats_user"),
 	path("private/post_match_data/", generate_match_data_stats, name="update_match_stats_user"),
 	path("private/post_tournament_data/", generate_tournament_data_stats, name="update_tournament_stats_user"),
-	path("", get_user_stats),
+	path("<int:player_id>", get_user_stats, name="get_user_stats"),
 ]
--- backend/stats/stats/views_public_api.py
+++ backend/stats/stats/views_public_api.py
@@ -1,14 +1,18 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.renderers import JSONRenderer
 from .models import Statistics
+from .serializer import StatisticsSerializer
 
 @api_view(['GET'])
 def get_user_stats(request, player_id):
-    try:
-        stats = Statistics.objects.get(player_id=player_id)
-    except Statistics.DoesNotExist:
-        return Response({"error": "Statistics not found for the given player_id"}, status=status.HTTP_404_NOT_FOUND)
+	try:
+		stats = Statistics.objects.get(player_id=player_id)
+	except Statistics.DoesNotExist:
+		return Response({"error": "Statistics not found for the given player_id"}, status=status.HTTP_404_NOT_FOUND)
 
-    serializer = StatisticsSerializer(stats)
-    return Response(serializer.data, status=status.HTTP_200_OK)
+	serializer = StatisticsSerializer(stats)
+	json_data = JSONRenderer().render(serializer.data)
+	print(json_data)
+	return Response(serializer.data, status=status.HTTP_200_OK)
