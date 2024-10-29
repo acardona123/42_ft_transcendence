48de224d8aee6f65d736367289604f0dcc26796d
QuentinThierry <qthierry2@gmail.com>
2024-10-29 15:25:21 +0100

    feat: design profile

--- frontend/modal_contents/profile/scripts/friends/friends_list.js
+++ frontend/modal_contents/profile/scripts/friends/friends_list.js
@@ -295,6 +295,7 @@ function update_friend_list_front()
 		const empty_text = document.createElement('p');
 		empty_text.textContent = "There is no friend to display yet."
 		empty_text.style.textAlign = "center";
+		empty_text.style.color = "var(--lavender)";
 		friends_list.appendChild(empty_text);
 		return ;
 	}
