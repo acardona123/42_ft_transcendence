c7ead43e95cc1f62258783c9c7ff275c9e530407
Alex Cardona <42.alex.cardona@gmail.com>
2024-10-31 00:51:58 +0100

    fix: tournament match final ai vs i was not working

--- frontend/modal_contents/tournament_round_program/scripts/start_round_matches.js
+++ frontend/modal_contents/tournament_round_program/scripts/start_round_matches.js
@@ -1,12 +1,18 @@
 
 async function start_first_match(match_data){
-	close_modal('modal-tournament-round-program', undefined, undefined, false);
+	close_modal('modal-tournament-round-program', undefined, false);
 	open_modal('modal-game', undefined, undefined, false);
 	start_tournament_match(match_data);
 }
 
-async function jump_to_next_round(){
-	init_modal_tournament_round_program();
+async function jump_to_next_round_or_top1(){  //can throw
+	const  go_to_tournament_winner_modal = await was_last_round();  //can throw
+	if (go_to_tournament_winner_modal){
+		close_modal('modal-tournament-round-program', reset_game, false);
+		display_tournament_winner();
+	} else {
+		init_modal_tournament_round_program();
+	}
 }
 
 async function tournament_start_round(){
@@ -16,8 +22,8 @@ async function tournament_start_round(){
 			start_first_match(first_match_data.data);
 			return;
 		} else if (first_match_data.status == 200){
-			create_popup("Warning: this round was only composed of bot's matches, here is the next round", 10000, 4000, HEX_GREEN, HEX_GREEN_HOVER);
-			jump_to_next_round();
+			create_popup("Warning: this round was only composed of bot's matches, it has been automatically completed. Lets continue", 10000, 4000, HEX_GREEN, HEX_GREEN_HOVER);
+			await jump_to_next_round_or_top1();  //can throw
 		} else {
 			throw Error;
 		}
